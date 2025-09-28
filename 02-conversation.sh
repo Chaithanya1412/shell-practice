ce6cabe112690b924e599ec34440182d9ec12f72
chaithanya <chaithanya123456789@gmail.com>
2025-09-28 23:24:49 +0530

    msg

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "ramesh:: hi suresh,how are you?"
+echo "suresh:: hello raamesh,  I am good. How are you?"
+echo "remesh:: what are you learning"
+echo "suresh:: i am  learning shell-script"
