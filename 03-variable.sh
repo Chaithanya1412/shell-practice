49b379a5b42aca4346802e0561330e89b01d33c6
chaithanya <chaithanya123456789@gmail.com>
2025-09-28 23:41:33 +0530

    msg

---
+++ 03-variable.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# no space before and after equal
+person1=john
+person2=raheem
+
+echo "$person1:: hi $person2,how are you?"
+echo "$person2:: hello $person1,  I am good. How are you?"
+echo "$person1:: what are you learning"
+echo "$person2: i am  learning shell-script $person1"
