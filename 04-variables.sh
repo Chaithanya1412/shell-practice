b5674d4cf336ffb806e34f98b34ce6aedd31fbc1
chaithanya <chaithanya123456789@gmail.com>
2025-09-30 21:26:21 +0530

    variables

---
+++ 04-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+person1=$1
+person2=$2
+echo "$person1:: welcome to alaska $person2"
+echo "$person2:: Thank you $person1, Finally not on Zoom Webinar"
+echo "$person1:: So, do we have a deal"
+echo "$person2:: Yes, if you make Ukraine joins Russia"
