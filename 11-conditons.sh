aa9fd9a73773653743967ca2139ab8acbce637a2
Chaithanya <buchaiagarichaithanya@gmail.com>
2025-10-12 21:17:21 +0530

    variables

--- 11-conditons.sh
+++ 11-conditons.sh
@@ -4,7 +4,7 @@ echo "enter the number:"
 read NUMBER
 
 if [$(($NUMBER % 2)) -eq 0] then
-    echo "Given number $number is EVEN"
+    echo "Given number $NUMBER is EVEN"
 else
-    echo "given number $number is ODD"
+    echo "given number $NUMBER is ODD"
 fi
